1be78f4d6f7d152fe67cd2426d02ba67e10f37e3
Yago Carlos Fernandez Gou <yagocarlos.fernandez@estudiants.urv.cat>
2020-11-30 10:09:22 +0100

    progM: reubicar diferents tipus de segments garlic_itm_mem.s

--- GARLIC_OS/source/garlic_itcm_mem.s
+++ GARLIC_OS/source/garlic_itcm_mem.s
@@ -26,17 +26,17 @@
 	@; cambio de las direcciones de memoria que se tienen que ajustar
 _gm_reubicar:
 	push {r0-r12, lr}
-		@; Agafa valor de la pila a r4 
+	
+		ldr r4, [r13, #56] 				@; r4 = (13 regs + lr)*4 
 	
 		ldr r12, [r0, #DESP_E_SHOFF]	@; r12 = e_shoff 
 		add r12, r12, r0 				@; r12 = e_shoff + file_content
 		
-		ldrh r4, [r0, #DESP_E_SHENTSIZE] @; r4 = e_shentsize 
 		ldrh r5, [r0, #DESP_E_SHNUM]	 @; r5 = e_shnum
 		sub r5, r5, #1 					 @; r5 = e_shnum - 1, restamos 1 para dec. hasta 0 
 		
-		@; while r5 >= 0 {
-		.L_recorre_seccions: 
+		@; do while r5 >= 0 {
+	.L_recorre_seccions: 
 		ldrh r7, [r0, #DESP_E_SHENTSIZE] @; r7 = e_shentsize 
 		mla r6, r5, r7, r12			@; r6 = file_content + e_shoff + (r5*sizeof(Elf32_Shdr))
 		
@@ -49,7 +49,7 @@ _gm_reubicar:
 		sub r7, #SIZE_REL				@; 	r7 = r7 - sizeof(Elf32_Rel), restamos 1 para dec. hasta 0  
 		
 		ldr r8, [r6, #DESP_SH_OFFSET]	@; 	r8 = sectionHeader->offset 
-		.L_recorre_seccio_SHT_REL: 
+	.L_recorre_seccio_SHT_REL: 
 										@; while (r7 >= 0) {
 		add r9, r8, r7						@; r9 = sectionHeader->offset + (r7*sizeof(Elf32_Rel)
 		add r9, r0							@; r9 = r9 + file_content 
@@ -59,23 +59,40 @@ _gm_reubicar:
 		cmp r10, #TYPE_R_ARM				@; Mascara 00000010
 		bne .L_fi_recorre_seccio_SHT_REL
 											@; if(relocator_r_info & 2) {
-		.L_reubica_ARM_ABS32: 
+	.L_reubica_ARM_ABS32: 
 		ldr r10, [r9]							@; r10 = relocator->r_offset 
 		sub r10, r1								@; r10 = relocator->r_offset - p_paddr 
 		add r10, r2								@; r10 = relocator->r_offset - p_paddr + INI_MEM 
 		
 		ldr r11, [r10]							@; r11 = Mem[r10]
-		sub r11, r1								@; r11 = r11 - p_paddr
-		add r11, r2								@; r11 = r11 + INI_MEM
-		str r11, [r10]							@; Mem[r10] = r11 
 		
-								 				
-		.L_fi_recorre_seccio_SHT_REL: 
+		@; Comprobar en cas segment dades carregat abans que codi en gestio de memoria
+		
+												@; if(r3 == 0 || Mem[r10] < r3) {
+		cmp r3, #0								@; 		.L_reubica_segment_codi
+		beq .L_reubica_segment_codi
+		
+		cmp r11, r3
+		blo .L_reubica_segment_codi 			
+												@; } else .L_reubica_segment_dades 
+	.L_reubica_segment_dades:
+		sub r11, r3 							@; r11 = r11 - p_paddr_dades
+		add r11, r4 							@; r11 = r11 + INI_MEM
+		b .L_guarda_reubicacio 
+		
+	.L_reubica_segment_codi: 
+		sub r11, r1								@; r11 = r11 - p_paddr_codi
+		add r11, r2								@; r11 = r11 + INI_MEM	
+	
+	.L_guarda_reubicacio:
+		str r11, [r10]							@; Mem[r10] = r11 
+	
+	.L_fi_recorre_seccio_SHT_REL: 
 		sub r7, #SIZE_REL		@; Restem mida realocador
 		cmp r7, #0
 		bge .L_recorre_seccio_SHT_REL 
 		
-		.L_fi_recorre_seccio: 
+	.L_fi_recorre_seccio: 
 		sub r5, #1				@; r5--; 
 		cmp r5, #0
 		bge .L_recorre_seccions	@; r5 >= 0
